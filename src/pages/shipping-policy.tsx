8338b37ba0173c97556ef21694b3e74a36d5b5a6
Reqvite <reqvite2233@gmail.com>
2023-05-14 14:25:26 +0300

    Update shipping-policy.tsx

--- src/pages/shipping-policy.tsx
+++ src/pages/shipping-policy.tsx
@@ -10,7 +10,7 @@ const ShippingPolicy = () => {
           are fully aware of our refund and return methods and procedures. These
           policies are applicable to domestic (United States) country.
         </p>
-        <p>A. 30 DAY RETURN POLICY</p>
+        <h3>A. 30 DAY RETURN POLICY</h3>
         <p>
           We have a 30-day return policy, which means you have 30 days after
           receiving your order to request a return. To be eligible for this
@@ -19,7 +19,7 @@ const ShippingPolicy = () => {
           clearly aware of our shipping policies and procedures. By ordering
           from this store you accept the policies contained herein.
         </p>
-        <p>A. SHIPPING COSTS</p>
+        <h3>A. SHIPPING COSTS</h3>
         <p>We do not charge for shipping in any region we service.</p>
         <p>B. TRANSIT, HANDLING &amp; ORDER CUT OFF TIME DOMESTICALLY</p>
         <p>
@@ -30,33 +30,33 @@ const ShippingPolicy = () => {
           or canceled. For information on the estimated arrival date, see the
           estimated shipping time reported at checkout.
         </p>
-        <p>C. Delivery Terms</p>
+        <h3>C. Delivery Terms</h3>
         <p>
           We use DPD, DHL, Yodel and Amazon Logistics. Carrier is determined on
           speed, item availability and weight.
         </p>
-        <p>D. CHANGE OF ADDRESS</p>
+        <h3>D. CHANGE OF ADDRESS</h3>
         <p>
           We cannot change the delivery address once it is in transit. If you
           need to change the place to deliver your order, please contact us
           within 24 hours of placing your order at{" "}
           <strong>contact@monoshopy.com</strong> or Call Us at +1 940 526 3328
         </p>
-        <p>E. CANCELLATIONS</p>
+        <h3>E. CANCELLATIONS</h3>
         <p>
           If you change your mind before you have received your order, we are
           able to accept cancellations up to the date it is dispatched. After
           this, we are unable to cancel or edit your order. For refunds, please
           refer to our refund policy.
         </p>
-        <p>F. Parcels Damaged In Transit</p>
+        <h3>F. Parcels Damaged In Transit</h3>
         <p>
           If you find a parcel is damaged in-transit, if possible, please reject
           the parcel from the courier and get in touch with our customer
           service. If the parcel has been delivered without you being present,
           please contact customer service with next steps.
         </p>
-        <p>G. Parcels Lost in Transit</p>
+        <h3>G. Parcels Lost in Transit</h3>
         <p>
           In relation to missing packages, if your item has not arrived within
           30 calendar days, please contact us at{" "}
